6cfa7231a59adf02b0ff3bc844a15487aa25464d
Lefebvre Steven <133742833+HamelenYakkington@users.noreply.github.com>
2024-10-27 10:34:35 +0100

    test tflite

--- sae_501/lib/main.dart
+++ sae_501/lib/main.dart
@@ -1,40 +1,103 @@
-// main.dart
+import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
-import 'camera.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:sae_501/object_detection.dart';
+import 'dart:io' show Platform;
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await initializeCameras();
-  runApp(KnightSight());
-}
+void main() => runApp(const MyApp());
 
-class KnightSight extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'HomePage',
-      home: HomePage(),
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.orange,
+        ),
+      ),
+      home: const MyHome(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
+class MyHome extends StatefulWidget {
+  const MyHome({super.key});
+
+  @override
+  State<MyHome> createState() => _MyHomeState();
+}
+
+class _MyHomeState extends State<MyHome> {
+  final imagePicker = ImagePicker();
+
+  ObjectDetection? objectDetection;
+
+  Uint8List? image;
+
+  @override
+  void initState() {
+    super.initState();
+    objectDetection = ObjectDetection();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Page Principale'),
+        title: Image.asset('assets/images/tfl_logo.png'),
+        backgroundColor: Colors.black.withOpacity(0.5),
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const CameraPage()),
-            );
-          },
-          child: Text('Caméra'),
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: Center(
+                child: (image != null) ? Image.memory(image!) : Container(),
+              ),
+            ),
+            SizedBox(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  if (Platform.isAndroid || Platform.isIOS)
+                    IconButton(
+                      onPressed: () async {
+                        final result = await imagePicker.pickImage(
+                          source: ImageSource.camera,
+                        );
+                        if (result != null) {
+                          image = objectDetection!.analyseImage(result.path);
+                          setState(() {});
+                        }
+                      },
+                      icon: const Icon(
+                        Icons.camera,
+                        size: 64,
+                      ),
+                    ),
+                  IconButton(
+                    onPressed: () async {
+                      final result = await imagePicker.pickImage(
+                        source: ImageSource.gallery,
+                      );
+                      if (result != null) {
+                        image = objectDetection!.analyseImage(result.path);
+                        setState(() {});
+                      }
+                    },
+                    icon: const Icon(
+                      Icons.photo,
+                      size: 64,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
