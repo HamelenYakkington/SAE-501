bd554199f3dd105b4f85b65ba44fef0f686a2aa7
Lefebvre Steven <133742833+HamelenYakkington@users.noreply.github.com>
2024-11-29 17:17:41 +0100

    show links of the api in the nav

--- startDocker.bat
+++ startDocker.bat
@@ -4,3 +4,4 @@ cd /d "%~dp0\api" || (echo "The folder 'api' does not exist." && exit /b 1)
 docker compose up -d || (echo "Failed to start containers." && exit /b 2)
 cd ..
 echo "Containers are running!"
+echo "Home's api there => http://localhost:8081/"
