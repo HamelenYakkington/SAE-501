afa1d38bec765603575d370fe51e836e43ec86d3
JulienMaa <julien.maaroufi@gmail.com>
2024-12-21 16:20:10 +0100

    api image
     (first steps)

---
+++ api/src/src/Exception/InvalidBase64ImageException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Exception;
+
+use Exception;
+
+class InvalidBase64ImageException extends Exception
+{
+    public function __construct(string $message = "Invalid Base64 format.", int $code = 0, \Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+}
