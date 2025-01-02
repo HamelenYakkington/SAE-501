5b5ab5845ba2e234c0bc9bc847f157cad778fec1
Lefebvre Steven <133742833+HamelenYakkington@users.noreply.github.com>
2025-01-02 19:34:00 +0100

    Update info_constants.dart

--- app-mobile/lib/constants/info_constants.dart
+++ app-mobile/lib/constants/info_constants.dart
@@ -2,9 +2,11 @@ import 'package:sae_501/entity/info.dart';
 
 class InfoConstant {
   static final List<Info> infos = [
-    Info("Titre 1", "Ceci est le contenu de la première info."),
-    Info("Titre 2", ""),
-    Info("Titre 3",
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
-  ];
+  Info("KnightSight", "Cette application est un projet étudiant BUT3 du Saulcy de Metz en France."),
+  Info("Principe", "Cette application utilise un modèle IA (Yolo8) afin de détecter des objets sur un flux vidéo ou une image statique."
+  "Elle permet également l'envoi de ces reconnaissances à un serveur en utilisant une API, ainsi d'autres utilisateurs pourront consulter les"
+  " détections."),
+  Info("API", "L'API est dockérisée et donc peut être facilement et rapidement hébergée. Elle utilise le framework PHP Symfony qui possède une"
+  " communauté large et solide, des dépendances existantes et leurs documentations permettent l'implémentation facile de solutions."),
+  Info("Groupe", "Le groupe d'élèves est composé de 4 étudiants, tous en développement informatique, en 3ème année.")];
 }
