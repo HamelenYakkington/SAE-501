77b591d0a7a5014cd686bad5fb240d6535d05a9f
Lefebvre Steven <133742833+HamelenYakkington@users.noreply.github.com>
2025-01-05 16:15:18 +0100

    fix register

--- app-mobile/lib/view/register.dart
+++ app-mobile/lib/view/register.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:sae_501/constants/validator.dart';
 import 'package:sae_501/services/api_service.dart';
+import 'package:sae_501/view/widget/button_return_custom.dart';
 import 'package:sae_501/view/widget/header_custom.dart';
 import 'package:sae_501/view/widget/footer_custom.dart';
 import 'package:sae_501/constants/view_constants.dart';
@@ -8,7 +9,6 @@ import 'package:sae_501/view/widget/input_text_custom.dart';
 import 'package:sae_501/view/widget/button_custom_gradient.dart';
 
 class Register extends StatefulWidget {
-
   const Register({Key? key}) : super(key: key);
 
   @override
@@ -66,90 +66,96 @@ class RegisterState extends State<Register> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: ViewConstant.backgroundScalfold,
-      body: Container(
-        color: ViewConstant.backgroundApp,
-        child: SafeArea(
-          child: SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  const SizedBox(height: 25),
-                  customHeader(
-                      'KnightSight', 'assets/images/chess_lense_logo.webp'),
-                  const SizedBox(height: 25),
-                  Form(
-                    key: _formKey,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        CustomTextInput(
-                          labelText: 'Nom',
-                          controller: _lastNameController,
-                          keyboardType: TextInputType.name,
-                          validator: Validator.lastNameValidator,
-                        ),
-                        const SizedBox(height: 16),
-                        CustomTextInput(
-                          labelText: 'Prenom',
-                          controller: _nameController,
-                          keyboardType: TextInputType.name,
-                          validator: Validator.nameValidator,
-                        ),
-                        const SizedBox(height: 16),
-                        CustomTextInput(
-                          labelText: 'Email',
-                          controller: _emailController,
-                          keyboardType: TextInputType.emailAddress,
-                          validator: Validator.emailValidator,
-                        ),
-                        const SizedBox(height: 16),
-                        CustomTextInput(
-                          obscure: true,
-                          labelText: 'Mot de passe',
-                          controller: _passwordController,
-                          keyboardType: TextInputType.text,
-                          validator: Validator.passwordValidator,
-                        ),
-                        const SizedBox(height: 16),
-                        CustomTextInput(
-                          obscure: true,
-                          labelText: 'Confirmation Mot de passe',
-                          controller: _passwordConfirmationController,
-                          keyboardType: TextInputType.text,
-                          validator: (value) => Validator.confirmPasswordValidator(
-                              value, _passwordController),
-                        ),
-                        const SizedBox(height: 16),
-                        Center(
-                          child:customGradientButton(
-                            context: context,
-                            text: "Inscription",
-                            onPressed: _submitForm,
-                            gradient: const LinearGradient(
-                              colors: [
-                                ViewConstant.gradientColorLightBlue,
-                                ViewConstant.gradientColorDarkBlue
+        resizeToAvoidBottomInset: true,
+        backgroundColor: ViewConstant.backgroundScalfold,
+        body: Container(
+          color: ViewConstant.backgroundApp,
+          child: SafeArea(
+            child: Stack(
+              children: [
+                Column(
+                  children: [
+                    const SizedBox(height: 25),
+                    customHeader(
+                        'KnightSight', 'assets/images/chess_lense_logo.webp'),
+                    const SizedBox(height: 25),
+                    Expanded(
+                      child: SingleChildScrollView(
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Form(
+                            key: _formKey,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                CustomTextInput(
+                                  labelText: 'Nom',
+                                  controller: _lastNameController,
+                                  keyboardType: TextInputType.name,
+                                  validator: Validator.lastNameValidator,
+                                ),
+                                const SizedBox(height: 16),
+                                CustomTextInput(
+                                  labelText: 'Prenom',
+                                  controller: _nameController,
+                                  keyboardType: TextInputType.name,
+                                  validator: Validator.nameValidator,
+                                ),
+                                const SizedBox(height: 16),
+                                CustomTextInput(
+                                  labelText: 'Email',
+                                  controller: _emailController,
+                                  keyboardType: TextInputType.emailAddress,
+                                  validator: Validator.emailValidator,
+                                ),
+                                const SizedBox(height: 16),
+                                CustomTextInput(
+                                  obscure: true,
+                                  labelText: 'Mot de passe',
+                                  controller: _passwordController,
+                                  keyboardType: TextInputType.text,
+                                  validator: Validator.passwordValidator,
+                                ),
+                                const SizedBox(height: 16),
+                                CustomTextInput(
+                                  obscure: true,
+                                  labelText: 'Confirmation Mot de passe',
+                                  controller: _passwordConfirmationController,
+                                  keyboardType: TextInputType.text,
+                                  validator: (value) =>
+                                      Validator.confirmPasswordValidator(
+                                          value, _passwordController),
+                                ),
+                                const SizedBox(height: 16),
+                                Center(
+                                  child: customGradientButton(
+                                    context: context,
+                                    text: "Inscription",
+                                    onPressed: _submitForm,
+                                    gradient: const LinearGradient(
+                                      colors: [
+                                        ViewConstant.gradientColorLightBlue,
+                                        ViewConstant.gradientColorDarkBlue
+                                      ],
+                                      begin: Alignment.topLeft,
+                                      end: Alignment.bottomRight,
+                                    ),
+                                    textColor: Colors.white,
+                                  ),
+                                ),
                               ],
-                              begin: Alignment.topLeft,
-                              end: Alignment.bottomRight,
                             ),
-                            textColor: Colors.white,
                           ),
                         ),
-                      ],
+                      ),
                     ),
-                  ),
-                  const SizedBox(height: 16),
-                  customFooter(),
-                ],
-              ),
+                    customFooter(),
+                  ],
+                ),
+                customReturnButton(context),
+              ],
             ),
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
