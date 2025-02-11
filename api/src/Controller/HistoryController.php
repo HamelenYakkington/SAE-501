fe39f8091aa774a672e355775fc5e1da48248bba
c0rv3t <tom.tomdu57ttt@gmail.com>
2025-02-12 00:55:15 +0100

    Historique fonctionnel par utilisateur
    L'historique permet de voir les photos prises depuis l'application mobile, on peut donc voir nos photos avec les labels de nos photos sur cette page
    Prérequis: une connexion à l'API

--- api/src/Controller/HistoryController.php
+++ api/src/Controller/HistoryController.php
@@ -2,30 +2,38 @@
 
 namespace App\Controller;
 
+use App\Entity\Image;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HistoryController extends AbstractController
 {
-    #[Route("/history", name:"history")]
-    public function index()
-    {
-        $uploadDir = $this->getParameter('kernel.project_dir') . '/public/uploads';
-        $imagesDir = $uploadDir . '/images';
-        $labelsDir = $uploadDir . '/labels';
 
-        $files = scandir($imagesDir);
+    #[Route("/history", name: "history")]
+    public function index(EntityManagerInterface $em): Response
+    {
+        $user = $this->getUser();
+        if (!$user) {
+            throw $this->createAccessDeniedException('You must be logged in to view your history.');
+        }
+
+        $images = $em->getRepository(Image::class)->findBy(['user' => $user]);
+
         $uploads = [];
 
-        foreach ($files as $file) {
-            if (preg_match('/\.(png|jpg|jpeg)$/i', $file)) {
-                $annotationFile = preg_replace('/\.(png|jpg|jpeg)$/i', '.txt', $file);
-                $boxes = [];
+        foreach ($images as $image) {
+            $imageFile = $image->getPathImage();
+            $labelFile = $this->getParameter('kernel.project_dir') . '/public/' . $image->getPathLabel();
+            $boxes = [];
 
-                if (file_exists($labelsDir . '/' . $annotationFile)) {
-                    $lines = file($labelsDir . '/' . $annotationFile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-                    foreach ($lines as $line) {
-                        list($class, $xCenter, $yCenter, $width, $height) = explode(' ', $line);
+            if (file_exists($labelFile)) {
+                $lines = file($labelFile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+                foreach ($lines as $line) {
+                    $parts = explode(' ', $line);
+                    if (count($parts) === 5) {
+                        list($class, $xCenter, $yCenter, $width, $height) = $parts;
                         $boxes[] = [
                             'class'    => $class,
                             'x_center' => (float)$xCenter,
@@ -35,12 +43,12 @@ class HistoryController extends AbstractController
                         ];
                     }
                 }
-
-                $uploads[] = [
-                    'image' => '/uploads/images/' . $file,
-                    'boxes' => $boxes,
-                ];
             }
+
+            $uploads[] = [
+                'image' => $imageFile,
+                'boxes' => $boxes,
+            ];
         }
 
         return $this->render('history/index.html.twig', [
